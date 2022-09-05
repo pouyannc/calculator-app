ac76ebcd9511063f53e641b45e8ffa9a2f700497
Pouyan Chamanian <pouyannc@gmail.com>
2022-09-05 18:10:02 -0400

    Add proper spacing between operands and ints

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function buttonAction (e) {
     console.log(e.target.textContent)
-    if (e.target.textContent == "AC") display.textContent = "0";
-    else if (e.target.textContent == "=") operate(display.textContent);
-    else if (display.textContent == 0) display.textContent = e.target.textContent;
-    else display.textContent += e.target.textContent;
+    const btnValue = e.target.textContent;
+    if (btnValue == "AC") display.textContent = "0";
+    else if (btnValue == "=") operate(display.textContent);
+    else if (display.textContent == 0) display.textContent = btnValue;
+    else if (regexpInts.test(btnValue)) display.textContent += " " + btnValue + " ";
+    else display.textContent += btnValue;
 }
 
 function operate (equation) {
     
 }
+
+const regexpInts = /[\+\-\*\/\%]/ig
 
 let buttons = document.querySelectorAll("button");
 let display = document.querySelector(".display");
