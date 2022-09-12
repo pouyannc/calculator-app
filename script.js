16c565ce6a456489ef5019d21c57fcafe4bbb3ab
Pouyan <pouyannc@gmail.com>
2022-09-12 19:11:34 -0400

    Add style and some extra conditions

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
 function buttonAction (e) {
-    console.log(e.target.textContent)
     const btnValue = e.target.textContent;
-    if (btnValue == "CLEAR") display.textContent = "0";
+    
+    if (btnValue == "CLEAR") {
+        display.textContent = "0";
+        oneDecimalPnt = false;
+    }
     else if (btnValue == "DELETE") {
         const displayArr = display.textContent.split("");
         if (displayArr[displayArr.length-1] == " ") displayArr.splice(displayArr.length-3,3);
+        else if (displayArr.length == 1) displayArr[0] = 0; 
         else displayArr.pop();
         display.textContent = displayArr.join("");
     }
     else if (btnValue == "=") {
-        let answer = evaluate(display.textContent.split(" "));
-        displayAns.textContent = answer;
+        if (display.textContent != 0) {
+            let answer = evaluate(display.textContent.split(" "));
+            if (typeof answer == "string") displayAns.textContent = answer;
+            else displayAns.textContent = +answer.toFixed(8);
+        }
     }
-    else if (display.textContent == 0) display.textContent = btnValue;
-    else if (regexpInts.test(btnValue)) display.textContent += " " + btnValue + " ";
-    else display.textContent += btnValue;
+    else if (regexpInts.test(btnValue)) {
+        display.textContent += " " + btnValue + " ";
+        oneDecimalPnt = false;
+    }
+    else if (btnValue == ".") {
+        if (oneDecimalPnt == false) {
+            display.textContent += btnValue;
+            oneDecimalPnt = true;
+        }
+    }
+    else {
+        if (display.textContent == 0 && oneDecimalPnt == false) display.textContent = btnValue;
+        else display.textContent += btnValue;
+    }
 }
 
 function evaluate (equationArr) {
     let equationLen = equationArr.length;
     if (equationLen === 1) return equationArr[0];
 
-    let firstMulDivIndex = equationArr.findIndex(e => (e === "*" || e === "/"));
+    let firstMulDivIndex = equationArr.findIndex(e => (e === "x" || e === "÷"));
     if (firstMulDivIndex === -1) {
         let firstAddSubIndex = equationArr.findIndex(e => (e === "+" || e === "-"));
         if (firstAddSubIndex === -1) return "Err";
@@ -31,25 +49,30 @@ function evaluate (equationArr) {
             return evaluate (equationArr);
         }
     } else {
-        let ans = operate[equationArr[firstMulDivIndex]](equationArr[firstMulDivIndex-1], equationArr[firstMulDivIndex+1]);
-        equationArr.splice(firstMulDivIndex-1, 3, ans);
-        return evaluate (equationArr);
+        if (equationArr[firstMulDivIndex] === "÷" && equationArr[firstMulDivIndex + 1] == 0) return "Undefined";
+        else {
+            let ans = operate[equationArr[firstMulDivIndex]](equationArr[firstMulDivIndex-1], equationArr[firstMulDivIndex+1]);
+            equationArr.splice(firstMulDivIndex-1, 3, ans);
+            return evaluate (equationArr);
+        }
     } 
 
 }
 
 const operate = {
-    "+": (a,b) => parseInt(a) + parseInt(b),
-    "-": (a,b) => parseInt(a) - parseInt(b),
-    "*": (a,b) => parseInt(a) * parseInt(b),
-    "/": (a,b) => parseInt(a) / parseInt(b),
+    "+": (a,b) => parseFloat(a) + parseFloat(b),
+    "-": (a,b) => parseFloat(a) - parseFloat(b),
+    "x": (a,b) => parseFloat(a) * parseFloat(b),
+    "÷": (a,b) => parseFloat(a) / parseFloat(b),
 } 
 
-const regexpInts = /[\+\-\*\/\%]/ig;
+const regexpInts = /[\+\-\x\÷\%]/ig;
 
 let buttons = document.querySelectorAll("button");
 let display = document.querySelector(".display");
 let displayAns = document.querySelector(".displayAns");
+
+let oneDecimalPnt = false;
 
 buttons.forEach(button => button.addEventListener("click", buttonAction));
 
